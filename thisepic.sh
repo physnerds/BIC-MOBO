70e32757d0454cca7e8b530a0ef2c9b623446546
physnerds <amitbashyal@gmail.com>
2025-12-24 18:03:53 -0500

    Redundant but updated anyway

--- thisepic.sh
+++ thisepic.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 export DETECTOR=epic
-#export DETECTOR_PATH=/opt/software/linux-x86_64_v2/epic-git.ab4f651214846e110534c1eda28ac0187f5ccf4a_main-re6jir3c4wa7q4bnb5slsoemz6jmarmz/share/epic
-export DETECTOR_PATH=$AIDE_WORKDIR/share/epic
+#export DETECTOR_PATH=/opt/detector/epic-main/share/epic
+export DETECTOR_PATH=${PWD}/share/epic
 export DETECTOR_CONFIG=${1:-epic}
-export DETECTOR_VERSION=25.12.0
+export DETECTOR_VERSION=25.12.0-3-gdf5195a2f
 
 ## Warn is not the right name (this script is sourced, hence $1)
 if [[ "$(basename ${BASH_SOURCE[0]})" != "thisepic.sh" ]]; then
@@ -14,7 +14,7 @@ fi
 
 ## Export detector libraries
 if [[ "$(uname -s)" = "Darwin" ]] || [[ "$OSTYPE" == "darwin"* ]]; then
-        export DYLD_LIBRARY_PATH="/opt/software/linux-x86_64_v2/epic-git.ab4f651214846e110534c1eda28ac0187f5ccf4a_main-re6jir3c4wa7q4bnb5slsoemz6jmarmz/lib${DYLD_LIBRARY_PATH:+:$DYLD_LIBRARY_PATH}"
+        export DYLD_LIBRARY_PATH="/opt/software/linux-x86_64_v2/epic-git.df5195a2f85e7cf63ec69b81b40fbca774010f83_main-2wzfab2p3w3qvasjgrh4t43t4aem3asx/lib${DYLD_LIBRARY_PATH:+:$DYLD_LIBRARY_PATH}"
 else
-        export LD_LIBRARY_PATH="/opt/software/linux-x86_64_v2/epic-git.ab4f651214846e110534c1eda28ac0187f5ccf4a_main-re6jir3c4wa7q4bnb5slsoemz6jmarmz/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+        export LD_LIBRARY_PATH="/opt/software/linux-x86_64_v2/epic-git.df5195a2f85e7cf63ec69b81b40fbca774010f83_main-2wzfab2p3w3qvasjgrh4t43t4aem3asx/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 fi
